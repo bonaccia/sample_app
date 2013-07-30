07ef92f2aafd2e43919dde688ab173addc5745ab
bonaccia <obonaccia@gmail.com>
2013-07-30 16:51:41 +0200

    halfway done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,14 +16,14 @@ describe "StaticPages" do
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
     it "should have the right links" do
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Sign up'))
       click_link "About"
       expect(page).to have_title(full_title('About Us'))
       click_link "Help"
       expect(page).to have_title(full_title('Help'))
       click_link "Contact"
       expect(page).to have_title(full_title('Contact'))
-#      click_link "Sign up now!"
-#      expect(page).to have_title(full_title('Sign up'))
     end
   end
 
