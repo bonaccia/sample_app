da84b77d420e217b0fdba1fc44e1980d24ab7764
bonaccia <obonaccia@gmail.com>
2013-08-04 14:50:31 +0200

    finish sign in/out

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "squid"
+    email                 "harmless@decap.od"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
+
